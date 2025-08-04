31cb5fa7e36e86f52db1f557bcc613318b63db68
Himanshu singh <himanshusinghrajput1107@gmail.com>
2025-08-04 22:18:22 +0530

    Fetch user channel profile with Many Details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -408,6 +408,75 @@ const updateCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, updatedUser, "Cover Image updated Succesfully"));
 });
 
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+  const {username}  = req.params;
+  if(!username?.trim()){
+    throw new ApiError(400,"Username is required");
+  }
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel", // We will match the _id from User to channel of subscription (to get the who have subscribed to you)
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        // This will give the details of all those channels which i have subscribed
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        ChannelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+            then: true,
+            else: false
+          }
+        }
+      },
+    },
+    {
+      // Project only the necessary data
+      $project: {
+        fullname: 1,
+        username : 1,
+        avatar : 1,
+        coverImage:1,
+        ChannelsSubscribedToCount:1,
+        isSubscribed:1,
+        email:1,
+    
+      }
+    }
+  ]);
+  if(!channel?.length){
+    throw new ApiError(404,"Channel not found");
+  }
+
+  return res.
+    status(200).json(new ApiResponse(
+      200,channel[0],"Channel profile fetched succesfully"
+    ))
+})
+
 export {
   registerUser,
   loginUser,
