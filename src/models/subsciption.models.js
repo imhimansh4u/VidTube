648febef77edef7db55ab47690cbed9972787d98
Himanshu singh <himanshusinghrajput1107@gmail.com>
2025-08-13 23:08:01 +0530

    Some new Comments added

--- src/models/subsciption.models.js
+++ src/models/subsciption.models.js
@@ -2,12 +2,14 @@ import mongoose, { Schema } from "mongoose";
 
 const SubscriptionSchema = new Schema(
   {
+    // for a user who is logged in VidTube the subscriber field will help him to get all the channels he has subscribedTo
     subscriber: {
-      type: Schema.Types.ObjectId, // This will give , which channel does the user has subscribed
+      type: Schema.Types.ObjectId,  //This field will store the ID of the logged-in user who is performing the subscription action.
       ref: "User",
     },
+    // For a user who is logged in VidTube the channel field will give him all His subscribers count (bcs he is listed here)
     channel: {
-      type: Schema.Types.ObjectId, // This will give the subscriber of that user
+      type: Schema.Types.ObjectId,  //This field will store the ID of the user whose channel is being subscribed to.
       ref: "User",
     },
   },
